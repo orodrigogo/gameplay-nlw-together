5e55eceea9e7023a6bf7be349f0d4cf7b5f958eb
Lenivene Bezerra <lenivene@msn.com>
2021-06-24 17:04:10 -0300

    fix: data owner correct name

--- src/components/Guild/index.tsx
+++ src/components/Guild/index.tsx
@@ -39,7 +39,7 @@ export function Guild({data, ...rest}: Props){
             </Text>
 
             <Text style={styles.type}>
-              { data.owen ? 'Administrador' : 'Convidado'}
+              { data.owner ? 'Administrador' : 'Convidado'}
             </Text>
           </View>
         </View>
